71eb709e476b9b5b2cdfca928dfce47d8a3501b1
GonzaTorres99 <gonzalotorres317@gmail.com>
2024-12-09 13:59:00 -0300

    seccion agregada y ajustes responsive

---
+++ script.js
@@ -0,0 +1,2 @@
+const code = '<iframe src="https://zeno.fm/player/liga-de-radios-escolares-pba" frameborder="0" scrolling="no"></iframe>';
+    document.getElementById("iframe-code").textContent = code;
